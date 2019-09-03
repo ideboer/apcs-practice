d441763a6966c128f4bfabb0855ecc2bffbcd880
Isabella DeBoer <isabella@songdeboer.net>
2019-09-03 12:28:46 -0600

    finalized tests

--- SignUp.java
+++ SignUp.java
@@ -5,24 +5,27 @@ public class SignUp
     public static void main( String[] args)
     {
         Scanner scan = new Scanner( System.in );
-        String = username, password, first_char;
-        String[] used_username = new String[]{"admin person", "jimbob234", "skater_gurl17", "gary"};
+        String username, password, first_char, not_last_two;
     
         
         System.out.println("Enter your username: ");
-        username = scan.nextString();
+        username = scan.nextLine();
 
-        if (username != used_username)
+        if (username.equals("admin person") || username.equals("jimbob234") || username.equals("skater_gurl17") || username.equals("gary")  )
+        {
+            System.out.println("That username is used");
+        }
+        else
         {
             System.out.println("Enter your password: ");
-            password = scan.nextString();
+            password = scan.nextLine();
 
             first_char = password.substring(0,1);
-            not_last_two = password.substring( (password.length() - 1), password.length() );
+            not_last_two = password.substring( 0, ( password.length() - 1 ) );
 
-            if (first_char == "a" || first_char == "e" || first_char == "i" || first_char == "o" || first_char == "u" || first_char == "A" || first_char == "E" || first_char == "I" || first_char == "O" || first_char == "U" );
+            if (first_char.equals("a") || first_char.equals("e") || first_char.equals("i") || first_char.equals("o") || first_char.equals("u") || first_char.equals("A") || first_char.equals("E") || first_char.equals("I") || first_char.equals("O") || first_char.equals("U") )
             {
-                if (password.contains("#") || password.contains("!") || password.contains("$") || password.contains("&") || password.contains("*"));
+                if (password.contains("#") || password.contains("!") || password.contains("$") || password.contains("&") || password.contains("*"))
                 {
                     if (password.contains(username))
                     {
@@ -30,8 +33,14 @@ public class SignUp
                     }
                     else
                     {
-                        if (not_last_two.contains("0") || last_two.contains("1") || last_two.contains("2") || last_two.contains("3") || last_two.contains("4") || last_two.contains("5") || last_two.contains("6") || last_two.contains("7") || last_two.contains("8") || last_two.contains("9")  );
-
+                        if (not_last_two.contains("0") || not_last_two.contains("1") || not_last_two.contains("2") || not_last_two.contains("3") || not_last_two.contains("4") || not_last_two.contains("5") || not_last_two.contains("6") || not_last_two.contains("7") || not_last_two.contains("8") || not_last_two.contains("9") )
+                        {
+                            System.out.println("Your password may only contain numbers in the last two characters.");
+                        }
+                        else
+                        {
+                            System.out.println("Sign up successful.");
+                        }
                     }
                 }
                 else
@@ -43,10 +52,7 @@ public class SignUp
             {
                 System.out.println("Your password must begin with a vowel");
             }
-        }
-        else
-        {
-            System.out.println("That username is used");
+              
         }
     }
 }
